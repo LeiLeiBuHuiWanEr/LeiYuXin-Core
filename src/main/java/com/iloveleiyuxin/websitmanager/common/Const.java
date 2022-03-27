82b6e2e02d0c3d6370d8ce01fc78a7f1f118b101
DwenDwen <liguangyao@shinow.com>
2022-03-27 22:54:39 +0800

    commit

--- src/main/java/com/iloveleiyuxin/websitmanager/common/Const.java
+++ src/main/java/com/iloveleiyuxin/websitmanager/common/Const.java
@@ -8,6 +8,6 @@ package com.iloveleiyuxin.websitmanager.common;
  */
 public class Const {
 
-    public final static String LEIYUXIN_CORE_VERSION = "0.1.4 MU5735 REST IN PEACE!";
+    public final static String LEIYUXIN_CORE_VERSION = "0.1.4 MU5735 REST IN PEACE";
     public final static String KAPTCHA_KEY = "kaptcha";
 }
